7095bed1abee37108c4cf82c7a7671f7f7f41d98
helloworldwriter <writer@helloworldwriter.com>
2020-10-02 18:16:58 +0300

    Improve intro, drawer, add social icons

--- lib/common/app_strings.dart
+++ lib/common/app_strings.dart
@@ -3,12 +3,21 @@ import 'package:horatiu_me/pages/home.dart';
 class AppStrings {
   static const String siteName = 'Horațiu Tănescu';
 
+  static const String mailtoUrl = 'mailto:contact@horatiu.me';
 
-  static const Map<MenuAction, String> menuActions = {
-    MenuAction.reset: 'Reset counter',
-    MenuAction.share: 'Share...',
-    MenuAction.rate: 'Rate app',
-    MenuAction.help: 'Help',
-  };
+  static const String newTooltip = 'Featured app';
+  static const String newUrl = 'https://www.aurelitec.com/colorveil/windows/';
+
+  static const String collectsReviewsTitle = 'horatiu.collects.reviews';
+  static const String collectsReviewsUrl = 'https://collects.reviews';
+
+  static const String githubTitle = 'GitHub';
+  static const String githubUrl = 'https://github.com/helloworldwriter';
+
+  static const String linkedinTitle = 'LinkedIn';
+  static const String linkedinUrl = 'https://www.linkedin.com/in/horatiudotme';
+
+  static const String facebookTitle = 'Facebook';
+  static const String facebookUrl = 'https://www.facebook.com/horatiu.me';
 
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,35 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:horatiu_me/common/app_strings.dart';
+import 'package:horatiu_me/common/custom_icons.dart';
 import 'package:horatiu_me/pages/home_features_tab.dart';
 import 'package:horatiu_me/pages/home_intro_tab.dart';
-import 'package:horatiu_me/pages/home_words_tab.dart';
+import 'package:horatiu_me/utils/utils.dart';
 import 'package:horatiu_me/widgets/link_list_tile.dart';
-import 'package:horatiu_me/widgets/link_text_span.dart';
 
 import 'home_works_tab.dart';
 
-/// Overflow menu items enumeration.
-enum MenuAction { reset, share, rate, help }
-
 class MyHomePage extends StatelessWidget {
-  /// Performs the tasks of the popup menu items (reset, share, rate, and help).
-  void popupMenuSelection(MenuAction item) {
-    switch (item) {
-      case MenuAction.reset:
-        break;
-      case MenuAction.share:
-        break;
-      case MenuAction.rate:
-        break;
-      case MenuAction.help:
-        break;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    // final bool isPortrait = MediaQuery.of(context).size.height >= 500;
-
     return DefaultTabController(
       length: 3,
       child: Scaffold(
@@ -51,54 +32,22 @@ class MyHomePage extends StatelessWidget {
   /// Builds the app bar with the popup menu items.
   PreferredSizeWidget _buildAppBar() {
     return AppBar(
-      // titleSpacing: 0,
       title: Text(AppStrings.siteName),
-      // title: Row(
-      //   children: [
-      //     CircleAvatar(backgroundImage: AssetImage('images/horatiu2.png')),
-      //     SizedBox(width: 8.0),
-      //     Text(AppStrings.siteName),
-      //   ],
-      // ),
       actions: [
         IconButton(
-          icon: const Icon(Icons.share),
-          // icon: CircleAvatar(backgroundImage: AssetImage('images/horatiu-48.png'), ),
-          // icon: Image(image: AssetImage('images/horatiu2.png'), ),
-          tooltip: 'Share',
-          onPressed: () {},
+          icon: const Icon(Icons.new_releases),
+          tooltip: AppStrings.newTooltip,
+          onPressed: () => launchUrl(AppStrings.newUrl),
         ),
-        // PopupMenuButton<MenuAction>(
-        //   onSelected: popupMenuSelection,
-        //   itemBuilder: _buildMenuItems,
-        // ),
       ],
       bottom: TabBar(
         tabs: [
-          Tab(
-            text: 'Intro',
-          ),
-          Tab(
-            text: 'Works',
-          ),
-          Tab(
-            text: 'Features',
-          ),
+          Tab(text: 'Intro'),
+          Tab(text: 'Works'),
+          Tab(text: 'Features'),
         ],
       ),
     );
-  }
-
-  /// Builds the popup menu items for the app bar.
-  List<PopupMenuItem<MenuAction>> _buildMenuItems(BuildContext context) {
-    return MenuAction.values
-        .map(
-          (item) => PopupMenuItem<MenuAction>(
-            value: item,
-            child: Text(AppStrings.menuActions[item]),
-          ),
-        )
-        .toList();
   }
 
   Widget _buildDrawer(BuildContext context) {
@@ -108,72 +57,42 @@ class MyHomePage extends StatelessWidget {
           DrawerHeader(
             child: Text(
               'Links',
-              style: Theme.of(context).textTheme.headline6,
+              style: Theme.of(context).textTheme.headline6.copyWith(color: Colors.white),
             ),
-            // decoration: BoxDecoration(
-            //   color: Colors.black,
-            // ),
+            decoration: BoxDecoration(
+              color: Colors.black,
+            ),
           ),
-          LinkListTile(
-            title: Text('horatiu.collects.reviews'),
-            url: 'https://horatiu.collects.reviews',
-          ),
-          LinkListTile(
-            title: Text('poezii.horatiu.me'),
-            url: 'https://poezii.horatiu.me',
-          ),
-          LinkListTile(
-            title: Text('7.00 – 7.53 AM'),
-            url: 'https://web.archive.org/web/20091022095316/http://www.700753am.com/',
-          ),
-          Divider(),
-          LinkListTile(
-            title: Text('LinkedIn'),
-            url: 'https://www.linkedin.com/in/horatiudotme',
-          ),
-          LinkListTile(
-            title: Text('Facebook'),
-            url: 'https://www.facebook.com/horatiu.me',
-          ),
+          // LinkListTile(
+          //   title: Text('poezii.horatiu.me'),
+          //   url: 'https://poezii.horatiu.me',
+          // ),
+          // LinkListTile(
+          //   title: Text('7.00 – 7.53 AM'),
+          //   url: 'https://web.archive.org/web/20091022095316/http://www.700753am.com/',
+          // ),
+          // _buildLinkListTile(null, AppStrings.collectsReviewsTitle, AppStrings.collectsReviewsUrl),
           // Divider(),
+          _buildLinkListTile(CustomIcons.github_circled, AppStrings.githubTitle, AppStrings.githubUrl),
+          _buildLinkListTile(CustomIcons.linkedin_circled, AppStrings.linkedinTitle, AppStrings.linkedinUrl),
+          _buildLinkListTile(CustomIcons.facebook_circled, AppStrings.facebookTitle, AppStrings.facebookUrl),
         ],
       ),
     );
   }
 
-  /// Builds the two main floating action buttons for increment and decrement.
-  Widget _buildFABs(bool isPortrait) {
-    return Flex(
-      direction: isPortrait ? Axis.vertical : Axis.horizontal,
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
-        FloatingActionButton(
-          onPressed: () => {},
-          // tooltip: AppStrings.decrementTooltip,
-          child: const Icon(Icons.email_outlined),
-        ),
-        isPortrait ? const SizedBox(height: 16.0) : const SizedBox(width: 16.0),
-        FloatingActionButton(
-          onPressed: () => {},
-          // tooltip: AppStrings.incrementTooltip,
-          child: const Icon(Icons.link),
-        )
-      ],
+  Widget _buildLinkListTile(IconData icon, String title, String url) {
+    return ListTile(
+      title: Text(title),
+      leading: icon != null ? Icon(icon) : null,
+      onTap: () => launchUrl(url),
     );
   }
 
   Widget _buildFAB() {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: () => launchUrl(AppStrings.mailtoUrl),
       child: const Icon(Icons.email_outlined),
-      // icon: Padding(
-      //   padding: const EdgeInsets.only(top: 8.0),
-      //   child: const Image(
-      //     image: AssetImage('images/horatiu2.png'),
-      //     width: 48,
-      //   ),
-      // ),
-      // icon: CircleAvatar(backgroundImage: AssetImage('images/horatiu2.png')),
     );
   }
 }
