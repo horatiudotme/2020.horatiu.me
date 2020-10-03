3e1924597acf28ee439548967c08d8f497ce12d4
helloworldwriter <writer@helloworldwriter.com>
2020-10-03 18:28:26 +0300

    Start adding urls for feature items

--- lib/pages/home_features_tab.dart
+++ lib/pages/home_features_tab.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:horatiu_me/content/home_features.dart';
+import 'package:horatiu_me/content/home_feature_items.dart';
+import 'package:horatiu_me/utils/utils.dart';
 
 class HomeFeaturesTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: kContentFeatures.length + 1,
+      itemCount: kFeatureItems.length + 1,
       itemBuilder: (BuildContext context, int index) {
         return index == 0
             ? Padding(
@@ -19,8 +20,8 @@ class HomeFeaturesTab extends StatelessWidget {
                 ),
               )
             : ListTile(
-                title: Text(kContentFeatures[index - 1]),
-                onTap: () {},
+                title: Text(kFeatureItems[index - 1].title),
+                onTap: () => launchUrl(kFeatureItems[index - 1].url),
               );
       },
     );
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,9 +1,11 @@
 import 'package:url_launcher/url_launcher.dart';
 
 void launchUrl(String url) async {
-  if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    throw 'Could not launch $url';
+  if (url != null) {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
